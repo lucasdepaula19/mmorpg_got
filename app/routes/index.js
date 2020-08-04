f3dec41fbd774e2386b86e8b4eed4705d89f668e
lucasdepaula19 <lucasdepaula2012@gmail.com>
2020-08-04 20:54:31 -0300

    método autenticar

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,4 +3,9 @@ module.exports = function(application){
 		//res.send('Bem vindo a sua app NodeJS!');
 		application.app.controllers.index.index(application, req, res);
 	});
+
+	application.post('/autenticar', function(req, res){
+		//res.send('Bem vindo a sua app NodeJS!');
+		application.app.controllers.index.autenticar(application, req, res);
+	});
 }
